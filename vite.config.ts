b49cd327af112adc9cc2c8bb7ae90d39ce4a9f2a
bilalaniq <bilalaniq2@gmail.com>
2025-08-16 19:25:13 +0500

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/my-app/', // <--- add your repo name here
+  base: '/nullsect/', // <-- exact name of your repo
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], 
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
